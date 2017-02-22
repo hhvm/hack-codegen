6550c3164d5b945466b46a7a4ef03e80c979f994
Fred Emmott <fred@fredemmott.co.uk>
2017-02-21 18:11:49 -0800

    Make HackBuilderValues::set() and ::immSet() take T as arraykey
    This is already a requirement of creating sets; this just makes it possible
    for the typechecker to give a clearer message in some cases.
    
    fixes #36

--- src/key-value-render/HackBuilderValues.php
+++ src/key-value-render/HackBuilderValues.php
@@ -48,13 +48,13 @@ abstract final class HackBuilderValues {
     return new HackBuilderValueCollectionRenderer(ImmVector::class, $vr);
   }
 
-  public static function set<Tv>(
+  public static function set<Tv as arraykey>(
     IHackBuilderValueRenderer<Tv> $vr,
   ): IHackBuilderValueRenderer<Set<Tv>> {
     return new HackBuilderValueCollectionRenderer(Set::class, $vr);
   }
 
-  public static function immSet<Tv>(
+  public static function immSet<Tv as arraykey>(
     IHackBuilderValueRenderer<Tv> $vr,
   ): IHackBuilderValueRenderer<ImmSet<Tv>> {
     return new HackBuilderValueCollectionRenderer(ImmSet::class, $vr);
