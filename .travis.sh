7fd568830e04d1edb9555ab6b4aa6b33036cb7b3
Fred Emmott <fred@fredemmott.co.uk>
2017-05-02 19:59:22 -0700

    Use Docker on TravisCI; adds 3.19 testing

---
+++ .travis.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+hhvm --version
+echo hhvm.jit=0 >> /etc/hhvm/php.ini
+apt-get update -y
+apt-get install -y wget curl git
+curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+
+cd /var/source
+hhvm /usr/local/bin/composer install
+hh_server --check $(pwd)
+hhvm vendor/bin/phpunit tests/
+hhvm examples/dorm/codegen.php examples/dorm/demo/DormUserSchema.php
