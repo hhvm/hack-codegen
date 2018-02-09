0c086f7e6477c48b41f40e55c5c78557dd88012b
Fred Emmott <fe@fb.com>
2018-02-09 09:14:51 -0800

    Dont' use docker on travis

--- .travis.sh
+++ .travis.sh
@@ -2,21 +2,12 @@
 set -ex
 hhvm --version
 
-curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+composer install
 
-cd /var/source
-hhvm /usr/local/bin/composer install
+hh_client
 
+hhvm vendor/bin/phpunit tests/
+
+echo > .hhconfig
+rm -rf vendor/hhvm/hhast # avoid circular dependency when fixing things
 hh_server --check $(pwd)
-sed -i '/enable_experimental_tc_features/d' .hhconfig
-hh_server --check $(pwd)
-
-hhvm -d hhvm.php7.all=0 vendor/bin/phpunit test/
-hhvm -d hhvm.php7.all=1 vendor/bin/phpunit test/
-
-hhvm examples/dorm/codegen.php examples/dorm/demo/DormUserSchema.php
-
-HHVM_VERSION=$(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null);
-if [ $HHVM_VERSION -ge 32300 -a $HHVM_VERSION -lt 32400 ]; then
-  vendor/bin/hhast-lint
-fi
