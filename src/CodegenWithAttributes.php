fe4f4f48c98cf91340faa3649cad592098d97b4d
Fred Emmott <fe@fb.com>
2018-04-12 11:00:09 -0700

    Relicense to MIT

--- src/CodegenWithAttributes.php
+++ src/CodegenWithAttributes.php
@@ -1,10 +1,11 @@
 <?hh // strict
-/**
- * Copyright (c) 2017-present, Fred Emmott
- * All rights reserved.
+/*
+ *  Copyright (c) 2015-present, Facebook, Inc.
+ *  All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
  */
 namespace Facebook\HackCodegen;
 
