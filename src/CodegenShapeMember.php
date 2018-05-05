ea287c542c61a8e24c13803c25050fea91e2d2f1
Michael Hahn <mh@slack-corp.com>
2018-05-05 12:57:24 -0700

    Simplify CodegenShapeMember

--- src/CodegenShapeMember.php
+++ src/CodegenShapeMember.php
@@ -12,32 +12,24 @@
 
  final class CodegenShapeMember {
 
-   private ?string $string_type;
-   private mixed $shape_type;
-
    public function __construct(
      public string $name,
-     mixed $type,
+     private mixed $type,
      public bool $optional = false,
    ) {
      invariant(
        is_string($type) || $type instanceof CodegenShapeFuture || $type instanceof CodegenShape,
        "You must provide either a string or shape",
      );
-
-     if ($type instanceof CodegenShapeFuture || $type instanceof CodegenShape) {
-       $this->shape_type = $type;
-     } else {
-       $this->string_type = (string) $type;
-     }
    }
 
    public function getType(): string {
-     if ($this->shape_type instanceof ICodeBuilderRenderer) {
-       return $this->shape_type->render();
+     if ($this->type instanceof ICodeBuilderRenderer) {
+       return $this->type->render();
      }
 
-     invariant($this->string_type !== null, "Somehow type is null");
-     return $this->string_type;
+     invariant($this->type !== null, "Somehow type is null");
+     invariant(is_string($this->type), "Somehow type is not a string");
+     return $this->type;
    }
  }
