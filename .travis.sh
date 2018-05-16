13ae8f05d8d00b437e1246c6e422eaec4eb3ae39
Fred Emmott <fe@fb.com>
2018-05-16 14:29:45 -0700

    rebuild dorm on travis

--- .travis.sh
+++ .travis.sh
@@ -8,6 +8,11 @@ hh_client
 
 hhvm vendor/bin/phpunit test/
 hhvm vendor/bin/hhast-lint
+hhvm examples/dorm/codegen.php examples/dorm/demo/DormUserSchema.php
+if ! git diff --quiet; then
+  echo "Demo codegen not up to date."
+  exit 1
+fi
 
 echo > .hhconfig
 rm -rf vendor/hhvm/hhast # avoid circular dependency when fixing things
