30147cd84555aad38838c6f859deb31a5d8cc3c8
Fred Emmott <fe@fb.com>
2019-02-12 15:59:15 -0800

    test on docker

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
+php --version
 
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
 composer install
 
 hh_client
