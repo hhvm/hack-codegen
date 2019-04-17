a0e4ed99401e5416f39000bc8bb7d8a0dbda9506
Fred Emmott <fe@fb.com>
2019-04-17 13:23:09 -0700

    Make travis.sh use /bin/bash instead of /bin/sh
    `foo/{file1,file2}.php` is a bashism.
    
    Also add comment explaining why we're doing both that and regen'ing +
    diffing

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 apt update -y
 DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
@@ -21,8 +21,11 @@ if !(hhvm --version | grep -q -- -dev); then
   vendor/bin/hhast-lint
 fi
 
+# Check the codegen hasn't been modified; this is also done as part of the next
+# step, but we want to make sure the verify-signatures executable works
 bin/hh-codegen-verify-signatures \
   examples/dorm/demo/{DormUser.php,DormUserMutator.php}
+# Check that they're up-to-date
 hhvm examples/dorm/codegen.hack examples/dorm/demo/DormUserSchema.php
 if ! git diff --exit-code examples/; then
   echo "Demo codegen not up to date."
